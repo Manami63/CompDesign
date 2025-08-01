cc77a0c75b92bfae1de9f1bf2e22ede971a4cf73
Manami63 <manami6300@gmail.com>
2025-08-01 10:12:35 +0900

    create ID.v, IF.v and IM.v

--- verilog_lab/SingleClockMIPS/ID.v
+++ verilog_lab/SingleClockMIPS/ID.v
@@ -1,13 +1,44 @@
-module ID(.CLK(CLK), .RST(RST), .Ins(Ins), .Wdata(Wdata),
-      .Rdata1(Rdata1), .Rdata2(Rdata2), .Ed32(Ed32));
-`include "common_param.vh"
+module ID(CLK, RST, Ins, Wdata, Rdata1, Rdata2, Ed32);
 
 input CLK, RST;
-input [31:0] Ins, Wdata;
+input [31:0] Ins, Wdata;      // Instruction(命令)
 output [31:0] Rdata1, Rdata2, Ed32;
 
-assign Radr1 = Ins[25:21];
-assign Radr2 = Ins[20:16];
-assign Wadr = Ins[20:16] 
-assign Wdata =
+reg [31:0] regs[0:31];
+
+// 命令を取り出す
+assign Op = Ins[31:26];       // opcode     
+assign Radr1 = Ins[25:21];    // rs reg addr
+assign Radr2 = Ins[20:16];    // rt reg addr
+assign Rd = Ins[15:11];       // rd reg addr
+assign Imm = Ins[15:0];       // immediate value          
+assign Wadr;                  // write address
+
+// レジスタから値を読む
+assing Rdata1 = regs[Radr1];
+assing Rdata2 = regs[Radr2];
+
+
+// 即値(I型命令)拡張 SE/UE
+assign Epd32 = (Op == 6'b001100 ||  // andi
+            Op == 6'b001101 ||      // ori
+            Op == 6'b001110) ?      // xori     
+            {16'b0, Imm} :          
+            {{16{Imm[15]}}, Imm};
+
+// 書き込み先レジスタを決定 MUX1
+assign Wdar = (OP == 6'b000011) ? 5'd31 :
+            (Op == 6'b000000) ? Rd :
+            Radr2;
+
+wire WE = (OP != 6'b000100) &&
+            (OP != 6'b000101) &&
+            (OP != 6'b000010);
+
+// 書き込み
+always @(posedge CLK) begin
+      if(WE && Wadr != 5'd0) begin
+            regs[Wadr] <= Wdata;
+      end
+end
 endmodule
--- verilog_lab/SingleClockMIPS/IF.v
+++ verilog_lab/SingleClockMIPS/IF.v
@@ -1,1 +1,24 @@
-IF IF0 (.CLK(CLK), .RST(RST), .newPC(newPC), .PC, .nextPC, .Ins);
+module IF(CLK, RST, newPC, PC, W_Ins, WE, nextPC, Ins);
+`include
+input CLK, RST, WE;
+input [31:0] newPC, W_Ins;
+output reg [31:0] PC; // reg?
+output [31:0] nextPC, Ins;
+
+// PCにある番地の命令を取得
+IM imem(
+    .addr(PC[31:2]),    //word addressing
+    .instr(Ins)
+);
+
+assign nextPC = PC + 4; //　つぎの命令アドレス
+
+    always @(posedge CLK or posedge RST) begin
+        if(RST)
+            PC <= 32'd0;
+        else
+            PC <= nextPC;
+    end
+endmodule
+
+// PCで命令を読んで，次のPCを計算
